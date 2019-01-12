eaf4692bb3e27fac9faa915fcd19a900612b43a4
Tobias Wältken <tobias.w@ltken.de>
2019-01-12 17:32:23 +0100

    Add fonts and color to editor

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ require.config({ paths: { 'vs': '../node_modules/monaco-editor/min/vs' }});
 
 require(['vs/editor/editor.main'], function() {
 	var editor = monaco.editor.create(document.getElementById('editor'), {
+		fontFamily: "Hack",
+		language: "python",
+		theme: "vs-dark",
 		value: "Hi, this is a example file!\n",
-		language: "python"
 	});
 });
