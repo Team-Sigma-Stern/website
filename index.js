2acc7e7da5adfb6748a069b24098df7e944bcc23
Tobias Wältken <tobias.w@ltken.de>
2019-01-12 18:37:54 +0100

    Add callback

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 require.config({ paths: { 'vs': '../node_modules/monaco-editor/min/vs' }});
 
+var editor = null;
+var active = null;
+
 require(['vs/editor/editor.main'], function() {
-	var editor = monaco.editor.create(document.getElementById('editor'), {
+	editor = monaco.editor.create(document.getElementById('editor'), {
 		fontFamily: "Hack",
 		language: "python",
 		theme: "vs-dark",
 		value: "Hi, this is a example file!\n",
 	});
 });
+
+function toggleFiles() {
+	if (active == null) {
+		active = "files";
+		document.getElementById("sidebar").style.display = "block";
+		editor.layout();
+	} else if (active == "files") {
+		active = null;
+		document.getElementById("sidebar").style.display = "none";
+		editor.layout();
+	} else {
+	}
+}
